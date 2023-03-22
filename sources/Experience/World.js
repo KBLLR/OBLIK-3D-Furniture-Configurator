fed448d4e04cc9dad1002d6adea9be1c3b5e6a6c
KBLLR <d-knight@tuta.io>
2023-03-22 03:26:41 +0100

    Fixed line height title, added cube to constructor

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -11,6 +11,7 @@ export default class World
         this.config = this.experience.config
         this.scene = this.experience.scene
         this.resources = this.experience.resources
+        this.cube = null;
         
         this.resources.on('groupEnd', (_group) =>
         {
@@ -19,37 +20,30 @@ export default class World
                 this.createTexturedCube();
             }
         })
+    }
 
-        //const textureAssets = assets[0].items.filter((asset) => asset.type === 'texture');
-      }
+    createTexturedCube() {
+      // Create materials for each face
+      const materials = [
+        new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture01, transparent: true, opacity: 1 }),
+        new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture02, transparent: true, opacity: 1 }),
+        new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture03, transparent: true, opacity: 1 }),
+        new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture04, transparent: true, opacity: 1 }),
+        new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture05, transparent: true, opacity: 1 }),
+        new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture06, transparent: true, opacity: 1 }),
+      ];
 
-      createTexturedCube() {
-        // Create materials for each face
-        const materials = [
-          new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture01, transparent: true, opacity: 1 }),
-          new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture02, transparent: true, opacity: 1 }),
-          new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture03, transparent: true, opacity: 1 }),
-          new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture04, transparent: true, opacity: 1 }),
-          new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture05, transparent: true, opacity: 1 }),
-          new THREE.MeshBasicMaterial({ map: this.resources.items.OBLIKtexture06, transparent: true, opacity: 1 }),
-        ];
+      materials[0].blending = THREE.NormalBlending;
+  
+      // Create a cube geometry and apply the materials
+      const geometry = new THREE.BoxGeometry();
+      this.cube = new THREE.Mesh(geometry, materials);
 
-        // materials[0].blending = THREE.NormalBlending;
-        // materials[1].blending = THREE.NormalBlending;
-        // materials[2].blending = THREE.NormalBlending;
-        // materials[3].blending = THREE.NormalBlending;
-        // materials[4].blending = THREE.NormalBlending;
-        // materials[5].blending = THREE.NormalBlending;
-    
-        // Create a cube geometry and apply the materials
-        const geometry = new THREE.BoxGeometry();
-        const cube = new THREE.Mesh(geometry, materials);
-
-        // Position the cube
-        cube.position.set(0, 0, 0)  // x, y, z
-
-        // Add the cube to the scene
-        this.scene.add(cube)
+      // Position the cube
+      this.cube.position.set(0, 0, 0)  // x, y, z
+      
+      // Add the cube to the scene
+      this.scene.add(this.cube)
     }
 
     resize()
@@ -58,6 +52,10 @@ export default class World
 
     update()
     {
+      if (this.cube) {
+        this.cube.rotation.x += 0.01;
+        this.cube.rotation.y += 0.01;
+      }
     }
 
     destroy()
